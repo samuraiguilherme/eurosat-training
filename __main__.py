47c7f5efe1a79270ec3c06a372f632e4ad5247cc
Guilherme Yagui <guilherme.takasawa@gmail.com>
2024-07-27 10:36:54 -0300

    initial commit

---
+++ __main__.py
@@ -0,0 +1,149 @@
+# from gen_dataset import GenDataset
+# ds = GenDataset("eurosat-simp")
+# ds()
+
+import os
+from datasets import load_dataset, ClassLabel, Sequence
+from transformers import (
+    DefaultDataCollator,
+    TFAutoModelForImageClassification,
+
+    AutoImageProcessor,
+    create_optimizer
+)
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras import layers
+from transformers.keras_callbacks import KerasMetricCallback
+import evaluate
+
+model_id = "google/vit-base-patch16-224-in21k"
+output_dir = './img-eurosat-model-trained'
+
+simp_ds = load_dataset('yaguilherme/eurosat-ds-test2')
+
+print('raw simp_ds: ',simp_ds)
+
+label_names = os.listdir('eurosat-simp')
+
+# Cast to ClassLabel
+simp_ds = simp_ds.cast_column("label", ClassLabel(names=label_names))
+
+train_test_ds = simp_ds['train'].train_test_split(test_size=0.2)
+print('train_test_ds: ',train_test_ds)
+
+labels = train_test_ds['train'].features['label'].names
+label2id, id2label = dict(), dict()
+for i, label in enumerate(labels):
+    label2id[label] = str(i)
+    id2label[str(i)] = label
+
+image_processor = AutoImageProcessor.from_pretrained(model_id)
+
+data_augmentation = tf.keras.Sequential(
+    [
+        layers.Resizing(image_processor.size['height'], image_processor.size['width']),
+        layers.Rescaling(1./255, offset=-1),
+        layers.RandomFlip('horizontal'),
+        layers.RandomRotation(factor=0.02),
+        layers.RandomZoom(height_factor=0.2, width_factor=0.2)
+    ],
+    name="data_augmentation"
+)
+val_data_augmentation = tf.keras.Sequential(
+    [
+        layers.CenterCrop(image_processor.size['height'], image_processor.size['width']),
+        layers.Rescaling(scale=1.0 /127.5, offset=-1)
+    ],
+    name='val_data_augmentation'
+)
+
+def convert_to_tf_tensor(image: Image):
+    np_image = np.array(image)
+    tf_image = tf.convert_to_tensor(np_image)
+    return tf.expand_dims(tf_image, 0)
+
+def preprocess_train(example_batch):
+    images = [
+        data_augmentation(convert_to_tf_tensor(image.convert('RGB'))) for image in example_batch['image']
+    ]
+    example_batch['pixel_values'] = [tf.transpose(tf.squeeze(image)) for image in images]
+    return example_batch
+
+def preprocess_val(example_batch):
+    images = [
+        val_data_augmentation(convert_to_tf_tensor(image.convert('RGB'))) for image in example_batch['image']
+    ]
+    example_batch['pixel_values'] = [tf.transpose(tf.squeeze(image)) for image in images]
+    return example_batch
+
+train_test_ds['train'].set_transform(preprocess_train)
+simp_ds['validation'].set_transform(preprocess_val)
+
+data_collator = DefaultDataCollator(return_tensors='tf')
+
+accuracy = evaluate.load('accuracy')
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+batch_size=16
+num_epochs=5
+num_train_steps=len(train_test_ds['train']) * num_epochs
+learning_rate=3e-5
+weight_decay_rate=0.01
+
+optimizer, lr_schedule = create_optimizer(
+    init_lr=learning_rate,
+    num_train_steps=num_train_steps,
+    weight_decay_rate=weight_decay_rate,
+    num_warmup_steps=0
+)
+
+model = TFAutoModelForImageClassification.from_pretrained(
+    model_id,
+    id2label=id2label,
+    label2id=label2id
+)
+
+tf_train_ds = train_test_ds['train'].to_tf_dataset(
+    columns='pixel_values',
+    label_cols='label',
+    shuffle=True,
+    batch_size=batch_size,
+    collate_fn=data_collator
+)
+tf_val_ds = simp_ds['validation'].to_tf_dataset(
+    columns='pixel_values',
+    label_cols='label',
+    shuffle=True,
+    batch_size=batch_size,
+    collate_fn=data_collator
+)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = [
+    tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy'),
+    tf.keras.metrics.SparseTopKCategoricalAccuracy(3, name='top-3-accuracy')
+]
+model.compile(
+    optimizer=optimizer,
+    loss=loss,
+)
+callbacks = [
+    KerasMetricCallback(metric_fn=compute_metrics, eval_dataset=tf_val_ds)
+]
+model.fit(
+    tf_train_ds,
+    validation_data=tf_val_ds,
+    callbacks=callbacks,
+    epochs=num_epochs
+)
+
+curpath = os.path.abspath(os.curdir)
+fulldirPath = os.path.join(curpath, 'eurosat-model-trained')
+
+model.save_pretrained(fulldirPath)
+image_processor.save_pretrained(fulldirPath)
---
+++ gen_dataset.py
@@ -0,0 +1,75 @@
+import os
+import datasets
+from datasets import Dataset, Features, Value, DatasetDict
+import pandas as pd
+from PIL import Image
+import math
+
+class GenDataset:
+    """
+    This class transforms the Eurosat directory into a dataset to be used in HF datasets
+    The goal is to transform this folder structure
+        https://madm.dfki.de/files/sentinel/EuroSAT.zip
+    in this https://huggingface.co/datasets/timm/eurosat-rgb/tree/main
+    folder structure (using parquet files, auto converted with push_to_hub):
+    """
+
+    def __init__(self, foldername):
+        curpath = os.path.abspath(os.curdir)
+        self.fulldirPath = os.path.join(curpath, foldername)
+        self.features = Features({
+            'label': Value(dtype='string'),
+            'image': datasets.Image(decode=True)
+        })
+    
+    def __call__(self):
+        train_labels = []
+        train_images = []
+        
+        validation_labels = []
+        validation_images = []
+
+        # slice each folder into 85% train / 15% validation
+        for subfolder in os.listdir(self.fulldirPath):
+            subfolder_path = os.path.join(self.fulldirPath, subfolder)
+            
+            # Check if the item is a directory
+            if os.path.isdir(subfolder_path):
+                # Get a list of all image files in the subfolder
+                image_files = [f for f in os.listdir(subfolder_path) if f.endswith((".jpg", ".png", ".jpeg"))]
+                
+                # Calculate the number of files to be moved to the "validation" folder (15% of the total)
+                num_validation_files = math.ceil(len(image_files) * 0.15)
+                
+                # Move the first 85% of the files to "train"
+                for i in range(0, len(image_files) - num_validation_files):
+                    source_path = os.path.join(subfolder_path, image_files[i])
+                    print(f"appending {image_files[i]} to train {subfolder}")
+                    train_labels.append(subfolder)
+                    train_images.append(datasets.Image().encode_example(Image.open(source_path)))
+
+                # Move the last 15% of the files to "validation"
+                for i in range(len(image_files) - num_validation_files, len(image_files)):
+                    source_path = os.path.join(subfolder_path, image_files[i])
+                    print(f"appending {image_files[i]} to validation {subfolder} ")
+                    validation_labels.append(subfolder)
+                    validation_images.append(datasets.Image().encode_example(Image.open(source_path)))
+
+        train_df = pd.DataFrame({
+            'label': train_labels,
+            'image': train_images
+        })
+
+        val_df = pd.DataFrame({
+            'label': validation_labels,
+            'image': validation_images
+        })
+
+        ds_dict = DatasetDict({
+            'train': Dataset.from_pandas(train_df, features=self.features),
+            'validation': Dataset.from_pandas(val_df, features=self.features)
+        })
+
+        print(ds_dict)
+        ds_dict.push_to_hub('yaguilherme/eurosat-ds-test2')
+        
